8e3823507392a51b70eb4e87eb4fdef061ca494d
Alexander <alekspavlov02@mail.ru>
2024-02-18 15:26:44 +0400

    add navigation

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
+import { TokenStorageService } from '../../service/token-storage.service';
+import { UserService } from '../../service/user.service';
+import {ImageUploadService} from '../../service/image-upload.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+selector: 'app-navigation',
+templateUrl: './navigation.component.html',
+styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
+isLoggedIn = false;
+isDataLoaded = false;
+user: User;
+userProfileImage: File;
+searchTerm: string;
+
+constructor(
+    private tokenService: TokenStorageService,
+    private userService: UserService,
+    private imageService: ImageUploadService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenService.getToken();
+
+    if(this.isLoggedIn) {
+      this.userService.getCurrentUser()
+        .subscribe(data => {
+          this.user = data;
+          this.isDataLoaded = true;
+        })
+
+      this.imageService.getProfileImage()
+        .subscribe(data => {
+          this.userProfileImage = data.imageBytes;
+        });
+    }
+  }
+
+  logout(): void {
+    this.tokenService.logOut();
+    this.router.navigate(['/login']);
+  }
+
+  onSearch(event): void {
+    event.preventDefault();
+
+    if (this.searchTerm) {
+      this.router.navigate(['/profile/', this.searchTerm]);
+      this.searchTerm = '';
+    }
+  }
+    formatImage(img: any): any {
+      if (img == null) {
+        return null;
+      }
+      return 'data:image/jpeg;base64,' + img;
+    }
 }
